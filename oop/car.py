4cc2f9cd191a80cb072ac2fb9ebc6bdb76a2178f
nashimoff <159990431+nashimoff@users.noreply.github.com>
2025-12-17 17:05:42 +0600

    a

--- oop/car.py
+++ oop/car.py
@@ -1,8 +1,35 @@
 class Car:
-    top_speed = 100
+    # top_speed = 100
+    # warnings = []
+    def __init__(self, starting_top_speed=100):
+        self.top_speed = starting_top_speed
+        self.warnings = []  
+    
+    def __repr__(self):
+        print('Printing...')
+        return 'Top Speed: {}, Warnings: {}'.format(self.top_speed, len(self.warnings))
+
+    def add_warning(self, warning_text):   
+        if len(warning_text) > 0:
+            self.warnings.append(warning_text) 
 
     def drive(self):
         print('I am driving but certainly not faster than {}'.format(self.top_speed))
 
+
 car1 = Car()
 car1.drive()
+
+# Car.top_speed = 200
+car1.add_warning('New warning')
+car1.warnings.append([])
+# print(car1.__dict__) 
+print(car1)
+
+car2 = Car(200)
+car2.drive()
+print(car2.warnings)
+
+car3 = Car(250)
+car3.drive()
+print(car3.warnings)
