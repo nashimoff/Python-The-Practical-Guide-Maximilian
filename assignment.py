877744dce0eb346546efc0c2b6cd3bc321ab6029
nashimoff <159990431+nashimoff@users.noreply.github.com>
2025-12-25 17:08:20 +0600

    a

--- assignment.py
+++ assignment.py
@@ -1,11 +1,14 @@
 # 1) Create a Food class with a “name” and a “kind” attribute as well as a “describe()” method (which prints “name” and “kind” in a sentence).
-# class Food:
-#     def __init__(self, name, kind):
-#         self.name = name
-#         self.kind = kind
+class Food:
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
 
-#     def describe(self):
-#         print('I am of type {} and my name is {}'.format(self.kind, self.name))
+    def __repr__(self):
+        return 'Name: {}, Kind: {}'.format(self.name, self.kind)
+
+    def describe(self):
+        print('I am of type {} and my name is {}'.format(self.kind, self.name))
 
 
 # banana = Food('Banana', 'fruit')
@@ -29,20 +32,27 @@
 # Food.kind = 'fruit'
 # Food.describe('meat','Pork')
 # 3) Create a  “Meat” and a “Fruit” class – both should inherit from “Food”. Add a “cook()” method to “Meat” and “clean()” to “Fruit”.
-class Food:
-    name = 'X'
-    kind = 'Y'
-    # def __init__(self, name, kind):
-    #     self.name = name
-    #     self.kind = kind
+class Meat(Food):
+    def __init__(self, name):
+        super().__init__(name, 'meat')
 
-    @staticmethod
-    def describe(kind, name):
-        print('I am of type {} and my name is {}'.format(kind, name))
+    def cook(self):
+        print('I am cooking!')
 
+class Fruit(Food):
+    def __init__(self, name):
+        super().__init__(name, 'fruit')
 
-Food.name = 'Banana'
-Food.kind = 'fruit'
-Food.describe('meat','Pork')
+    def clean(self):
+        print('I am cleaning!')
 
+banana = Fruit('Banana')
+banana.clean()
+banana.describe()
+
+pork = Meat('Pork')
+pork.cook()
+pork.describe()
 # 4) Overwrite a “dunder” method to be able to print your “Food” class.
+print(pork)
+print(banana)
